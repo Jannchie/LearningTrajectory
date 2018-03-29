0e34ae8ae287236784db9e6376d4e362f1510647
jannchie <jannchie@gmail.com>
2018-03-29 23:07:10 +0800

    数据库链接以及图形的显示
    完成了从数据库取出数据并以Json的格式发送到前端的部分。完成了将从数据库里取出的数据点在图形中显示出来的功能。

--- LearningTrajectory/LearningTrajectory/database.py
+++ LearningTrajectory/LearningTrajectory/database.py
@@ -1,5 +1,23 @@
+# coding=utf-8
 from LearningTrajectory import app
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+import configparser
+import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://x:x@1x/x'
+# 获取当前路径
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+
+"""
+    获取日志文件。
+
+    此处由于链接字符串中存在敏感信息，比如密码，用户名，服务器IP地址等，上传到GitHub上时为了保证信息安全，需要一个配置文件来存储之。在gitignore文件中加入此文件。
+"""
+def getConfig(section,key):
+    config = configparser.ConfigParser()
+    config.read(curr_dir + "\learningTrajectory.conf")
+    return config.get(section,key)
+
+JVTconnectionstring = getConfig("database","JVTconnectionstring")
+app.config['SQLALCHEMY_DATABASE_URI'] = JVTconnectionstring
 db = SQLAlchemy(app)
+
