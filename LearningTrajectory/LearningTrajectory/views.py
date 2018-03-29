617db38c40a2846ffdae9d4731a6df295e768332
jannchie <jannchie@gmail.com>
2018-03-29 22:57:23 +0800

    数据库链接

--- LearningTrajectory/LearningTrajectory/views.py
+++ LearningTrajectory/LearningTrajectory/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Routes and views for the flask application.
 """
@@ -7,47 +8,48 @@ from datetime import datetime
 from flask import render_template
 from LearningTrajectory import app
 from LearningTrajectory.database import db
-from LearningTrajectory.models.jp import Jp
+from LearningTrajectory.JapaneseVocabularyTrajectory import JapaneseVocabularyTrajectory
+import json
+
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+    return render_template('index.html',
+        title='主页',
+        year=datetime.now().year,)
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
+    return render_template('contact.html',
+        title='联系',
         year=datetime.now().year,
-        message='Welcome to contact.'
-    )
+        message='如果需要联系我')
 
 @app.route('/about')
 def about():
     """Renders the about page."""
 
-    return render_template(
-        'about.html',
-        title='About',
+    return render_template('about.html',
+        title='关于',
         year=datetime.now().year,
-        message='Something about me.'
-    )
+        message='我想说的一些话')
 
 @app.route('/japanese')
 def japanese():
-    j = Jp.query.all()
-
     """Renders the japanese page."""
-    return render_template(
-        'japanese.html',
-        data=j,
-        title='Japanese Learning Trajectory',
+    return render_template('japanese.html',
+
+        title='日语学习轨迹',
         year=datetime.now().year,
-        message='Record my Japanese progress.'
-    )
+        message='记录下我日语学习的进程')
+
+@app.route('/japaneseGraph')
+def japaneseGraph():
+    """Renders the japanese page."""
+    japaneseVocabularyTrajectory = JapaneseVocabularyTrajectory.query.all()
+    data = []
+    for row in japaneseVocabularyTrajectory:
+        data.append([row.date.strftime("%Y/%m/%d"),row.number])
+    return json.dumps(data)
