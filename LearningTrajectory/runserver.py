307f0d112c3e12ad1e708f930432ba06736acecd
jannchie <jannchie@gmail.com>
2018-04-22 11:14:08 +0800

    只兼容python3

--- LearningTrajectory/runserver.py
+++ LearningTrajectory/runserver.py
@@ -1,9 +1,9 @@
 """
 This script runs the MyLearningPath application using a development server.
 """
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+#import sys
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 
 from os import environ
 from LearningTrajectory import app
